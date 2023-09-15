03cb5c8ae40da5916acdd2c1c906bd190d099da9
kbabayevv <kamran.babayevv@gmail.com>
2023-09-15 22:54:48 +0400

    collections

---
+++ src/main/java/collections/comparator/Student.java
@@ -0,0 +1,34 @@
+package collections.comparator;
+
+public class Student {
+    private String name;
+    private Integer age;
+    private Long id;
+
+    public Student(String name, Integer age, Long id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id=" + id +
+                '}';
+    }
+}
