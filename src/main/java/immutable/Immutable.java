7626c31a7ba2ae9567e1d214a05d10b7cba64319
kbabayevv <kamran.babayevv@gmail.com>
2023-10-18 15:30:10 +0400

    implement immutable

--- src/main/java/immutable/Immutable.java
+++ src/main/java/immutable/Immutable.java
@@ -5,16 +5,13 @@ public final class Immutable {
     private final Integer age;
     private final StringBuilder sb;
 
-    private Immutable(String name, Integer age, StringBuilder sb) {
+    public Immutable(String name, Integer age, StringBuilder sb) {
         this.name = name;
         this.age = age;
         // mutable olan (StringBuilder, SringBuffer, Collectionlar ve s.) new ile vermek lazimdir
         this.sb = new StringBuilder(sb.toString());
     }
 
-    public static Immutable of(String name, Integer age, StringBuilder sb) {
-        return new Immutable(name, age, sb);
-    }
 
     public String getName() {
         return name;
@@ -31,15 +28,6 @@ public final class Immutable {
 
 class Test {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("salam");
-        Immutable m = Immutable.of("KAmran", 24, sb);
 
-        sb.append(" necesen");
-
-
-        System.out.println(m.getSb().hashCode());
-        m.getSb().append(" necesen");
-        System.out.println(m.getSb().hashCode());
     }
 }
