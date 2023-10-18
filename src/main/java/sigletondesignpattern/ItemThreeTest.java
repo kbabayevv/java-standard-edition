ee4fb11d00323bed40234f59d8113982eda6f14f
kbabayevv <kamran.babayevv@gmail.com>
2023-10-18 16:00:17 +0400

    lazy&eager and enum

---
+++ src/main/java/sigletondesignpattern/ItemThreeTest.java
@@ -0,0 +1,28 @@
+package sigletondesignpattern;
+
+public class ItemThreeTest {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+
+                }
+                System.out.println("Hashcode of Lazy: " + LazyCache.getInstance().hashCode());
+                System.out.println("Hashcode of Eager: " + EagerCache.getInstance().hashCode());
+                System.out.println("Hashcode of Enum: " + CacheSingleton.INSTANCE.hashCode());
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hashcode of Lazy: " + LazyCache.getInstance().hashCode());
+                System.out.println("Hashcode of Eager: " + EagerCache.getInstance().hashCode());
+                System.out.println("Hashcode of Enum: " + CacheSingleton.INSTANCE.hashCode());
+            }
+        }).start();
+    }
+}
