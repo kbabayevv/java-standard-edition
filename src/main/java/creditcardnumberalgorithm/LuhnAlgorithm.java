3fcafa67fa95f53734db378bf3d9343ccdc8a002
kbabayevv <kamran.babayevv@gmail.com>
2024-03-01 20:54:38 +0400

    luhn algorithm

--- src/main/java/creditcardnumberalgorithm/LuhnAlgorithm.java
+++ src/main/java/creditcardnumberalgorithm/LuhnAlgorithm.java
@@ -14,23 +14,24 @@ public class LuhnAlgorithm {
     }
 
     public static boolean isValid(String number) {
+        number = number.replaceAll("[^0-9]", "");
         int sum = 0;
-        boolean alternate = false;
+        boolean alternate = true;
 
-        for (int i = number.length() - 1; i >= 0; i--) {
+        for (int i = 0; i < number.length(); i++) {
             int digit = Character.getNumericValue(number.charAt(i));
 
             if (alternate) {
                 digit *= 2;
                 if (digit > 9) {
-                    digit -= 9;
+                    digit = digit / 10 + digit % 10;
                 }
             }
 
             sum += digit;
             alternate = !alternate;
         }
-
+        System.out.println("Sum is : " + sum);
         return (sum % 10 == 0);
     }
 }
